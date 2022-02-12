592f8e2a4c1573a19f57306d93f3aef083835a18
Zhen <zyuluna@gmail.com>
2022-02-11 23:53:00 -0800

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,16 @@ app.use(express.json());
 //     app.use(cors());
 // }
 app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
+
 // helmet helps set a variety of headers to better secure your app
 app.use(helmet({
     contentSecurityPolicy: false
